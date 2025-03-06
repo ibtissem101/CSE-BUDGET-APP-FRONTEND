e9cfa1fad615edcc87732795217145439a04c49e
User <issaadibtissem101@gmail.com>
2025-03-06 03:24:35 -0800

    fixed z-index issue

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -2,9 +2,9 @@
 
 const Navbar = () => {
   return (
-    <div className="md:hidden fixed  top-0 left-0 right-0 bg-white text-white p-4 z-40">
+    <div className="md:hidden fixed top-0 left-0 right-0 bg-white p-4 z-30">
       <div className="flex items-center gap-2">
-        <img src="/navBar/logo-purp.png" className="w-6 " alt="" />
+        <img src="/navBar/logo-purp.png" className="w-6" alt="" />
         <span className="text-2xl italic text-Violet-500 -tracking-4">
           Tracker
         </span>
