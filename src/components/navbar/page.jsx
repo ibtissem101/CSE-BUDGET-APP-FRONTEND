781526491d2e917b1e388a19fb41facb525c0d8b
User <issaadibtissem101@gmail.com>
2025-03-08 13:48:32 -0800

    merged navbar

---
+++ src/components/navbar/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <div className="md:hidden fixed top-0 left-0 right-0 bg-white p-4 z-30">
+      <div className="flex items-center gap-2">
+        <img src="/navBar/logo-purp.png" className="w-6" alt="" />
+        <span className="text-2xl italic text-Violet-500 -tracking-4">
+          Tracker
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
